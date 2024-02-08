0567b3607323339f780d7babd60b91d8ee5788f6
gpuslave <alenemt@yandex.ru>
2024-02-08 13:52:42 +0300

    Initial python bot steps

---
+++ test.py
@@ -0,0 +1,19 @@
+import json
+
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+# reading json file that contains secret token to a bot
+with open(r'variables.json') as json_token:
+  json_str = json_token.read()
+  var_dic = json.loads(json_str)
+  token = var_dic["token"]
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  pass
