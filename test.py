c22cce0fab41621f8806b459ad20a2d4c6f5a87a
gpuslave <alenemt@yandex.ru>
2024-02-11 08:12:04 +0300

    minor change

--- test.py
+++ test.py
@@ -5,10 +5,10 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
 # reading json file that contains secret token to a bot
-with open(r'variables.json') as json_token:
-  json_str = json_token.read()
-  var_dic = json.loads(json_str)
-  token = var_dic["token"]
+with open(r'variables.json', encoding='utf-8') as json_token:
+    json_str = json_token.read()
+    var_dic = json.loads(json_str)
+    token = var_dic["token"]
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -16,4 +16,4 @@ logging.basicConfig(
 )
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  pass
+    pass
